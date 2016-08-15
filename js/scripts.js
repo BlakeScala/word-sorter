8c90e67cd5bc58482d7af60479984964c34a36d4
Blake Scala and Garrett Boggs <blake@>
2016-08-15 16:16:36 -0700

    get branching and loops to work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     var paragraph = $("input#paragraph").val();
@@ -5,15 +6,14 @@ $(document).ready(function() {
     var a = [];
     var b = [];
     var c = [];
+    var newb = [];
+    var answer =[];
 
     prev = -1;
 
-    alert(paragraph);
 
     var newpara = paragraph.split(" ");
     newpara.sort();
-
-    alert(newpara);
 
     newpara.forEach(function(str){
       if(str !== newpara[prev])
@@ -29,17 +29,26 @@ $(document).ready(function() {
 
     });
 
-    alert(a);
-    alert(b);
+    var newb = b.slice();
+    newb.sort();
+    newb.reverse();
 
-    b.forEach(function(int){
-      c.push(int + " " + a[counter]);
-      counter += 1;
+    for(var i = 0; i < b.length ; i++)
+    {
+      for(var j = 0; j < b.length ; j++)
+      {
+        if(newb[i] === b[j])
+        {
+            answer.push(newb[i] + " " + a[j]);
+        }
+      }
+    }
+
+    answer = answer.filter(function(item, pos) {
+    return answer.indexOf(item) == pos;
     });
 
-
-
-    alert(c);
+    alert(answer);
 
 
     event.preventDefault();
